3b2f687f5d52f7b16ac3c109719c0ccfb3c633a2
Joe Lyons <josephjlyons90@gmail.com>
2021-03-04 17:36:05 -0500

    rework paths to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ const uuid = require('uuid');
 
 
 router.get('/api/notes', (req, res) => {
-    return res.send(notes);
+    res.send(notes);
 });
 
 router.post('/api/notes', (req, res) => {
@@ -16,9 +16,9 @@ router.post('/api/notes', (req, res) => {
         text: req.body.text
     };
 
-    fs.readFile('/db/db.json', (err, data) => {
+    fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-        let parsedNotes = JSON.parse(data);
+        const parsedNotes = JSON.parse(data);
         parsedNotes.push(newNote);
         fs.writeFile('./db/db.json', JSON.stringify(parsedNotes), err => {
             if (err) throw err;
@@ -32,8 +32,8 @@ router.delete('api/notes/:id', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
 
-        let parsedNotes = JSON.parse(data);
-        let filteredNotes = parsedNotes.filter((note) => note.id != noteId);
+        const parsedNotes = JSON.parse(data);
+        const filteredNotes = parsedNotes.filter((note) => note.id != noteId);
 
         fs.writeFile('./db/db.json', JSON.stringify(filteredNotes), err => {
             if (err) throw err;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,17 @@
 const path = require('path');
-const express = require('express');
 const router = require('express').Router();
 
 
-router.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname), '../index.html');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;

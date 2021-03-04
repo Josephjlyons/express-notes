1be22939eac6df064a59b683c6ec9782f6b712e2
Joe Lyons <josephjlyons90@gmail.com>
2021-03-04 17:34:07 -0500

    clean up structure

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 // const uuid = require('uuid');
 const path = require('path');   
-const axios = require('axios');
-const htmlRoutes = require('./routes/htmlRoutes');
-
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", apiRoutes); // make a new route if needed to fit file structure?
-app.use(express.static('public'));
+app.use('/', apiRoutes); 
 app.use('/', htmlRoutes);
 
 
